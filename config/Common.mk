741c72478306ecd3953e5a4ba6aa008040eab797
Axel Kohlmeyer <akohlmey@gmail.com>
2012-07-28 16:31:51 +0200

    different approach to get updates with commits

--- config/Common.mk
+++ config/Common.mk
@@ -9,16 +9,23 @@ OBJ=$(PPS:.F90=.o) $(SRC:.f90=.o)
 FCFLAGS=$(OPTFLAGS) $(ARCHFLAGS)
 ##########################################
 ##########################################
-default: $(EXE)
+default: version compile
+compile: $(EXE)
 
 clean:
 	-rm -f $(OBJ) $(EXE) *.mod .depend version.*
 
-.PHONY: clean default version
+.PHONY: clean compile default version
 ##########################################
 # generate version header
 version.f90: $(SRC) $(PPS) ../config/mkversion.sh ../config/Common.mk
 	@../config/mkversion.sh FC="$(FC)" FCFLAGS="$(FCFLAGS)"
+
+version:
+	@-git log -n 1 --pretty="     write (stdout,*) 'Last commit  : %H'" > .ver1
+	@-grep commit version.f90 > .ver2 2> /dev/null
+	@cmp -s .ver1 .ver2 || rm -f version.f90
+	@-rm  .ver1 .ver2
 
 ##########################################
 # pattern rules
