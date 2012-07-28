591150ac60293dae3cc2a191ddb249a2d8d838c3
Axel Kohlmeyer <akohlmey@gmail.com>
2012-07-28 12:12:37 +0200

    better handling of version header generation

--- config/Common.mk
+++ config/Common.mk
@@ -2,35 +2,41 @@
 ##########################################
 VPATH=../src
 EXE=asap-md.x
-SRC=main.f90 version.f90 kinds.f90 constants.f90 \
-	io.f90 threading.f90
+SRC=main.f90 kinds.f90 constants.f90 io.f90 \
+	threading.f90
 PPS=messages.F90
 OBJ=$(PPS:.F90=.o) $(SRC:.f90=.o)
-##########################################
 FCFLAGS=$(OPTFLAGS) $(ARCHFLAGS)
 ##########################################
-default: $(EXE)
-clean:
-	rm -f $(OBJ) $(EXE) *.mod .depend version.f90
 ##########################################
+default: $(EXE)
+
+clean:
+	rm -f $(OBJ) $(EXE) *.mod .depend version.*
+
+.PHONY: default clean version
+
+##########################################
+# generate version header
+version.f90: $(OBJ)
+	../config/mkversion.sh FC="$(FC)" FCFLAGS="$(FCFLAGS)"
+
+##########################################
+# pattern rules
 .SUFFIXES:
 .SUFFIXES: .o .f90 .F90
 
-.PHONY: default clean
-##########################################
 .F90.o:
 	$(FC) $(FPPFLAGS) -c $(FCFLAGS) $<
 
 .f90.o:
 	$(FC) -c $(FCFLAGS) $<
 
-$(EXE): $(OBJ)
+$(EXE): version.o $(OBJ)
 	$(FC) -o $@ $(FCFLAGS) $(LINKFLAGS) $^
 
 ##########################################
-version.f90: ../config/mkversion.sh
-	../config/mkversion.sh FC="$(FC)" FCFLAGS="$(FCFLAGS)"
-
-.depend: $(PPS) $(SRC)
+# dependency tracking
+.depend: $(PPS) $(SRC) version.f90
 	../config/mkdep.pl -o $@ -I ../src $^
 -include .depend
