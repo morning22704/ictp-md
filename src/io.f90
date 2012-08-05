46de2cf423538c367d7409bc4a7f8f97a552b94a
Axel Kohlmeyer <akohlmey@gmail.com>
2012-08-05 22:16:25 +0200

    add channels for reading and writing restarts

--- src/io.f90
+++ src/io.f90
@@ -3,11 +3,13 @@ module io
   implicit none
 
   private
-  public :: stdin, stdout, stderr, separator
+  public :: stdin, stdout, stderr, resin, resout, separator
 
   integer, parameter :: stdin  = 5
   integer, parameter :: stdout = 6
   integer, parameter :: stderr = 0
+  integer, parameter :: resin  = 15
+  integer, parameter :: resout = 16
   character(len=55), parameter :: &
     separator = '-------------------------------------------------------'
 end module io
