fa276c86c42ef1289d663f1b2e28d92ef24b4829
Axel Kohlmeyer <akohlmey@gmail.com>
2012-09-08 10:09:53 +0200

    bugfix: correctly shift coordinates with image flag updates

--- src/atoms.f90
+++ src/atoms.f90
@@ -415,15 +415,15 @@ contains
 
     if (d == 'x') then
        o = 1
-       x_s%x(i) = x_s%x(i) + n*d_one
+       x_s%x(i) = x_s%x(i) - n*d_one
        valid_x_r = .false.
     else if (d == 'y') then
        o = 11
-       x_s%y(i) = x_s%y(i) + n*d_one
+       x_s%y(i) = x_s%y(i) - n*d_one
        valid_x_r = .false.
     else if (d == 'z') then
        o = 21
-       x_s%z(i) = x_s%z(i) + n*d_one
+       x_s%z(i) = x_s%z(i) - n*d_one
        valid_x_r = .false.
     else
        o = 31
